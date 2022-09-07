b42ec2d31084a2d32d15c2fcb249ce62a7e3e92c
anderson.caldeira <anderson.caldeira@perinity.com>
2022-09-07 10:27:14 -0300

    Logo ao iniciar o projeto

--- src/main.c
+++ src/main.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    printf("Bem-vindo a Barsi!");
+    printf("Bem-vindo a\n");
+
+    printf(" ____                 _ \n");
+    printf("| __ )  __ _ _ __ ___(_)\n");
+    printf("|  _ \\ / _` | '__/ __| |\n");
+    printf("| |_) | (_| | |  \\__ \\ |\n");
+    printf("|____/ \\__,_|_|  |___/_|\n");
+    
 }
