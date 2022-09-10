4b361811db1b71a480be85657e6d823843dd3571
Esc9919 <Emersoncaldeira1999@hotmail.com>
2022-09-10 00:55:31 -0300

    criando login

---
+++ src/controller/login.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+    int opcao = 0;
+
+    char user[15] = "esc";
+    char loginuser[15];
+    char senha[15] = "1234";
+    char loginsenha[15];
+
+    printf("User: ");
+    scanf("%s", loginuser);
+
+    printf("Senha: ");
+    scanf("%s", loginsenha);
+
+    // printf("%s %s", loginuser, loginsenha);
+
+    if(strcmp(user, loginuser) == 0 && strcmp(senha, loginsenha) == 0) {
+        printf("acesso autorizado\n Bem Vindo a Barsi");
+    } else {
+        printf("acesso negado");
+    }
+
+
+}
