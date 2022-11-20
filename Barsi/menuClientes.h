cb525a3a4468f6a034c7ca71124df4fbddda36ee
Anderson Caldeira <anderson0caldeira@gmail.com>
2022-11-20 17:31:09 -0300

    Update menuClientes.h

--- Barsi/menuClientes.h
+++ Barsi/menuClientes.h
@@ -17,32 +17,24 @@ void menuClientes() {
 
         printf("Qual opcao deseja:\n");
         printf("------------------------------------------------------\n");
-<<<<<<< HEAD
         printf("1 --- Incluir cliente\n");
         printf("2 --- Alterar cliente\n");
         printf("3 --- Consultar cliente\n");
         printf("4 --- Excluir cliente\n");
         printf("5 --- Retornar ao menu\n");
-=======
-        printf("1 - Incluir cliente\n");
-        /*printf("2 - Alterar cliente\n");
-        printf("3 - Consultar cliente\n");
-        printf("4 - Excluir cliente\n");*/
-        printf("5 - Nível do investidor\n");
-        printf("6 - Retornar ao menu\n");
->>>>>>> e3e91ed4646a5bc6cb12e15476a5f8c9c1d787bf
-        printf("Digite uma opção: ");
+
+        printf("Digite uma opÃ§Ã£o: ");
         scanf("%d", &saida);
         printf("------------------------------------------------------\n");
 
         if(saida < 1 || saida <= 6){
             break;
         } else {
-            printf("Opção invalida, por favor digite uma opção válida!\n\n\n");
-            //Carlos - Acrescentei o registro do log caso o usuário tenha digitado uma opção invalida.
+            printf("OpÃ§Ã£o invalida, por favor digite uma opÃ§Ã£o vÃ¡lida!\n\n\n");
+            //Carlos - Acrescentei o registro do log caso o usuÃ¡rio tenha digitado uma opÃ§Ã£o invalida.
             FILE *logFile;
             logFile=fopen("log.txt", "a");
-            fprintf(logFile,"Usuário digitou uma opção invalida.\n");
+            fprintf(logFile,"UsuÃ¡rio digitou uma opÃ§Ã£o invalida.\n");
             fclose(logFile);
             system("pause");
         }
@@ -64,13 +56,10 @@ void menuClientes() {
         case 4:
             excluirClientes();
             break;
-<<<<<<< HEAD
-=======
 
         case 5:
             investidorNivel();
             break;
->>>>>>> master
 
         default:
             menuPrincipal();
